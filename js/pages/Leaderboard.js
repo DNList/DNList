4133541dcf26d19447dc893b8f56a65a05c87939
DeliriosNocturnosTeam <deliriosnocturnosteam@gmail.com>
2025-10-26 22:17:11 +0100

    Update Leaderboard.js

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -45,9 +45,9 @@ export default {
                     <div class="player">
                         <h1>#{{ selected + 1 }} {{ entry.user }}</h1>
                         <h3>{{ entry.total }}</h3>
-                        <h2 v-if="entry.verified.length > 0">Verified ({{ entry.verified.length}})</h2>
+                        <h2 v-if="entry.firstvictor.length > 0">First Victor ({{ entry.firstvictor.length}})</h2>
                         <table class="table">
-                            <tr v-for="score in entry.verified">
+                            <tr v-for="score in entry.firstvictor">
                                 <td class="rank">
                                     <p>#{{ score.rank }}</p>
                                 </td>
