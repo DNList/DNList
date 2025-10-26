c6045a2c89addd2df291f87bd361684e3963ecc7
DeliriosNocturnosTeam <deliriosnocturnosteam@gmail.com>
2025-10-26 22:01:27 +0100

    Update content.js

--- js/content.js
+++ js/content.js
@@ -57,17 +57,17 @@ export async function fetchLeaderboard() {
             return;
         }
 
-        // Verification
-        const verifier = Object.keys(scoreMap).find(
-            (u) => u.toLowerCase() === level.verifier.toLowerCase(),
-        ) || level.verifier;
-        scoreMap[verifier] ??= {
-            verified: [],
+        // FirstVictor
+        const firstvictor = Object.keys(scoreMap).find(
+            (u) => u.toLowerCase() === level.firstvictor.toLowerCase(),
+        ) || level.firstvictor;
+        scoreMap[firstvictor] ??= {
+            firstvictor: [],
             completed: [],
             progressed: [],
         };
-        const { verified } = scoreMap[verifier];
-        verified.push({
+        const { firstvictor } = scoreMap[firstvictor];
+        firstvictor.push({
             rank: rank + 1,
             level: level.name,
             score: score(rank + 1, 100, level.percentToQualify),
