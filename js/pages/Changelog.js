bb785c1ad013d6e1a241b5e2809512ccf888cb8c
DeliriosNocturnosTeam <deliriosnocturnosteam@gmail.com>
2025-10-27 18:06:57 +0100

    a

--- js/pages/Changelog.js
+++ js/pages/Changelog.js
@@ -4,22 +4,20 @@ export default {
     return {
       changelog: [
         {
-          date: "2025-10-27",
-          message:
-            "• Updated 'Calamity' to #2 on the list.<br>• Fixed incorrect record on 'Luna'.",
+          date: "27-10-2025",
+          message: "• Release of the DNList v1.1.0. Added the changelog page.<br>• Future Funk has been raised from #15 to #11, below Double Dash and above Multition (2P).",
           featured: true,
         },
         {
-          date: "2025-10-20",
-          message: "• Added new level 'Spectrum'.",
-        },
-        {
-          date: "",
-          message: "• Fixed leaderboard loading issue.",
+          date: "27-10-2025",
+          message:
+            "• Added (#6) Future Funk II, (#7) Game Time, (#8) Magma Bound, (#9) Stalemate, (#10) Double Dash, (#11) Multition (2P), (#12) Nine Circles, (#13) Forsaken Neon, (#14) White women, (#15) Future Funk, (#16) Megalovania, (#17) A Pretty Easy Demon, (#18) B, (#19) Deadlocked, (#20) Shapes and Beats to the list.",
+          featured: true,
         },
         {
           date: "26-10-2025",
-          message: "• Initial release of DNList v1.0.0.<br>• Added (#1) Doradura.",
+          message: "• Initial release of DNList v1.0.0.<br>• Added (#1) Doradura, (#2) Mizureta, (#3) Niwa, (#4) Troll level, (#5) Acu to the list.",
+          featured: true
         },
       ],
     };
