9d3dd922bd3ba3e6680d2a9de8195a219b4181fd
DeliriosNocturnosTeam <deliriosnocturnosteam@gmail.com>
2025-10-27 17:44:28 +0100

    as

--- js/pages/Changelog.js
+++ js/pages/Changelog.js
@@ -5,26 +5,27 @@ export default {
       changelog: [
         {
           date: "2025-10-27",
-          message: "• Updated 'Calamity' to #2 on the list.\n• Fixed incorrect record on 'Luna'.",
-          featured: true
+          message:
+            "• Updated 'Calamity' to #2 on the list.<br>• Fixed incorrect record on 'Luna'.",
+          featured: true,
         },
         {
           date: "2025-10-20",
-          message: "• Added new level 'Spectrum'."
+          message: "• Added new level 'Spectrum'.",
         },
         {
           date: "2025-10-15",
-          message: "• Fixed leaderboard loading issue."
+          message: "• Fixed leaderboard loading issue.",
         },
         {
           date: "2025-10-10",
-          message: "• Initial release of DNList v1.0.0."
-        }
-      ]
+          message: "• Initial release of DNList v1.0.0.",
+        },
+      ],
     };
   },
   template: `
-    <div class="page-changelog-container">
+    <main class="page-changelog-container">
       <div class="page-changelog">
         <div class="changelog-container">
           <div
@@ -32,12 +33,12 @@ export default {
             :key="index"
             :class="['changelog-entry', { featured: entry.featured }]"
           >
-            <div class="date">{{ entry.date }}</div>
-            <div class="message" v-html="entry.message.replace(/\\n/g, '<br>')"></div>
+            <p class="type-label-lg changelog-date">{{ entry.date }}</p>
+            <p class="changelog-message" v-html="entry.message"></p>
           </div>
         </div>
       </div>
-    </div>
-  `
+    </main>
+  `,
 };
 
