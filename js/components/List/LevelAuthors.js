b6ce5646143f03b598cdbe6ebea260999e7e1dbd
DeliriosNocturnosTeam <deliriosnocturnosteam@gmail.com>
2025-11-03 19:50:03 +0100

    a

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -4,10 +4,6 @@ export default {
             type: String,
             required: true,
         },
-        creators: {
-            type: Array,
-            required: false,
-        },
         verifier: {
             type: String,
             required: true,
@@ -15,13 +11,13 @@ export default {
     },
     template: `
         <div class="level-authors">
-            <template v-if="selfVerified">
+            <template v-if="author === verifier">
                 <div class="type-title-sm">Creator & Verifier</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
                 </p>
             </template>
-            <template v-else-if="creators.length === 0">
+            <template v-else>
                 <div class="type-title-sm">Creator</div>
                 <p class="type-body">
                     <span>{{ author }}</span>
@@ -31,29 +27,6 @@ export default {
                     <span>{{ verifier }}</span>
                 </p>
             </template>
-            <template v-else>
-                <div class="type-title-sm">Creators</div>
-                <p class="type-body">
-                    <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
-                        <span >{{ creator }}</span
-                        ><span v-if="index < creators.length - 1">, </span>
-                    </template>
-                </p>
-                <div class="type-title-sm">Verifier</div>
-                <p class="type-body">
-                    <span>{{ verifier }}</span>
-                </p>
-            </template>
-            <div class="type-title-sm">Publisher</div>
-            <p class="type-body">
-                <span>{{ author }}</span>
-            </p>
         </div>
     `,
-
-    computed: {
-        selfVerified() {
-            return this.author === this.verifier && this.creators.length === 0;
-        },
-    },
 };
