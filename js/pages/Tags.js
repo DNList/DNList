bf70fdae8ae1795d551d40cdd4627997f7e78044
DeliriosNocturnosTeam <deliriosnocturnosteam@gmail.com>
2025-11-04 17:23:11 +0100

    a

--- js/pages/Tags.js
+++ js/pages/Tags.js
@@ -1,6 +1,6 @@
 import { store } from "../main.js";
 import { fetchList } from "../content.js";
-import tags from "../data/tags.js" // your tags.js module
+import tags from "../components/List/Tags.js";
 import Spinner from "../components/Spinner.js";
 
 export default {
