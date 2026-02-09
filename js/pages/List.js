112d5e0cbedda741e71a35013782010253f21b14
DeliriosNocturnosTeam <deliriosnocturnosteam@gmail.com>
2026-02-09 22:28:26 +0100

    el antiguo cojones si no va me mato

--- js/pages/List.js
+++ js/pages/List.js
@@ -40,15 +40,7 @@ export default {
 
             <div class="level-container">
                 <div class="level" v-if="level">
-                    <h1 class="level-title">
-                        <img
-                            v-if="level.tags?.includes('hard_demon')"
-                            src="/assets/hard_demon.webp"
-                            class="difficulty-icon"
-                            alt="Hard Demon"
-                        />
-                        {{ level.name }}
-                    </h1>
+                    <h1 class="level-title">{{ level.name }}</h1>
                     <p
                         v-if="level.subtitle"
                         class="level-subtitle"
